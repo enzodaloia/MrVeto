b05b679d7104faa298ca046262945429934b156e
Mathys NOURRY <mathys.nourry@gmail.com>
2026-02-17 16:41:33 +0100

    feat: vet search bar with address autocomplete + connected to backend

--- src/Controller/SearchVetController.php
+++ src/Controller/SearchVetController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class SearchVetController extends AbstractController
 {
     #[Route('/front/search', name: 'app_search_vet')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $vets = $userRepository->findAllVets();
+
         return $this->render('search-vet/searchvet.html.twig', [
             'controller_name' => 'SearchVetController',
+            'vets' => $vets,
         ]);
     }
 }
