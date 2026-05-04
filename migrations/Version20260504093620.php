2e230593fb4f9867ccb89bbfadc7cbb288d6856c
Lucas Da Silveira <lucasdslvra@gmail.com>
2026-05-04 11:37:13 +0200

    fix : problème migration

--- migrations/Version20260504093620.php
+++ migrations/Version20260504093620.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260504092302 extends AbstractMigration
+final class Version20260504093620 extends AbstractMigration
 {
     public function getDescription(): string
     {
