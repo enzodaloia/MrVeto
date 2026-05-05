be153d6a0ba174507954862dc308df21c9a83d72
Enzo <enzo.daloiadignazio@gmail.com>
2026-05-05 17:52:20 +0200

    fix parce que ça a pété

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -38,9 +38,6 @@ class RegistrationFormType extends AbstractType
                 ->add('telephone')
                 ->add('adressecabinet')
                 ->add('siret')
-                ->add('numeroOrdreVeterinaire', TextType::class, [
-                    'required' => false,
-                ])
                 ->add('latitude', HiddenType::class, ['required' => false])
                 ->add('longitude', HiddenType::class, ['required' => false]);
         }
